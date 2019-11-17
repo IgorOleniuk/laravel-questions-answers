3e1dc397902da6c094b841f968918a3e3dfa25c8
Igor Oleniuk <igoroleniuk@Igors-MacBook-Pro.local>
2019-11-17 06:14:15 +0200

    votes answers and questions anf polymirphic relationships

--- app/Question.php
+++ app/Question.php
@@ -71,4 +71,19 @@ class Question extends Model
     public function getFavoritesCountAttribute() {
         return $this->favorites->count();
     }
+
+    public function votes()
+    {
+        return $this->morphToMany('App\User', 'votable');
+    }
+
+    public function upVotes()
+    {
+        return $this->votes()->wherePivot('vote', 1);
+    }
+
+    public function downVotes()
+    {
+        return $this->votes()->wherePivot('vote', -1);
+    }
 }
--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -2,13 +2,13 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Answer;
+use App\Model;
 use Faker\Generator as Faker;
 
 $factory->define(Answer::class, function (Faker $faker) {
     return [
         'body' => $faker->paragraphs(rand(3, 7), true),
         'user_id' => App\User::pluck('id')->random(),
-        'votes-count' => rand(0, 5),
+        //'votes_count' => rand(0, 5),
     ];
 });
